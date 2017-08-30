f74aab12cfeffb4a8a86a0c0309528f5588ce9ca
Toshiki Yagi <mrabbit@usagigoya.jp>
2017-08-30 18:30:09 +0900

    fix shift modifier

--- layouts/community/ergodox/pseudo_us/keymap.c
+++ layouts/community/ergodox/pseudo_us/keymap.c
@@ -69,10 +69,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Keymap 2: Pseudo US Layer */
     [PSEUDO_US] = KEYMAP(
         // left hand
-        KC_FN2,         KC_FN2,  KC_FN2, KC_FN2, KC_FN2, KC_FN2,   TG(MEDIA),
+        KC_FN2,         KC_FN2,  KC_FN2, KC_FN2, KC_FN2, KC_FN2, KC_ESC,
         KC_TAB,         KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_FN2,
         KC_LCTL,        KC_A,    KC_S,   KC_D,   KC_F,   KC_G,
-        OSM(MOD_LSFT),  KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   SHIFT(KC_8),
+        OSM(MOD_LSFT),  KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   S(KC_8),
         KC_HYPR,        KC_RCTL, KC_LALT,KC_LGUI,KC_MHEN,
                                             TG(MEDIA),    KC_PGUP,
                                                           KC_PGDN,
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      LALT(LCTL(KC_DEL)), KC_FN2,   KC_FN2,  KC_FN2,   KC_FN2,  KC_FN2, KC_FN2,
              KC_FN2,     KC_Y,     KC_U  ,  KC_I,     KC_O,    KC_P,   KC_FN2,
                          KC_H,     KC_J,    KC_K,     KC_L,    KC_FN2, KC_FN2,
-             SHIFT(KC_9),KC_N,     KC_M,    KC_FN2,   KC_FN2,  KC_SLSH,KC_FN2,
+             S(KC_9),    KC_N,     KC_M,    KC_FN2,   KC_FN2,  KC_SLSH,KC_FN2,
                                    KC_MEH,  KC_LEFT,  KC_DOWN, KC_UP,  KC_RGHT,
              KC_DEL,        TG(SYMBOL),
              KC_BSPC,
