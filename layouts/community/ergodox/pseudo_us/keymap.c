064c410b1da5a3dbb8c19e5d7ed472f4bda665a1
Toshiki Yagi <mrabbit@usagigoya.jp>
2017-09-04 11:17:01 +0900

    merged from origin

--- layouts/community/ergodox/pseudo_us/keymap.c
+++ layouts/community/ergodox/pseudo_us/keymap.c
@@ -57,7 +57,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,  KC_LBRC,
         KC_LCTL,        KC_A,    KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,  KC_LPRN,
+<<<<<<< Updated upstream
         KC_HYPR,        KC_RCTL, KC_LALT,KC_LGUI,TD(TD_LANG),
+=======
+        KC_HYPR,        KC_RCTL, KC_LALT,KC_LGUI,KC_LANG1,
+>>>>>>> Stashed changes
                                                        TG(MEDIA), KC_PGUP,
                                                                   KC_PGDN,
                                                KC_SPC, KC_RSFT,   LALT(KC_LSFT),
@@ -66,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_RBRC,                KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,    KC_MINS,
                                      KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT,
              KC_RPRN,                KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_BSLS,
-                                             KC_HENK,KC_LEFT,KC_DOWN,KC_UP,   KC_RGHT,
+                                             KC_LANG2,KC_LEFT,KC_DOWN,KC_UP,   KC_RGHT,
              KC_DEL,        TG(SYMBOL),
              KC_BSPC,
              KC_RALT,KC_LCTL, KC_ENT
@@ -78,7 +82,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_FN2,
         KC_LCTL,        KC_A,    KC_S,   KC_D,   KC_F,   KC_G,
         OSM(MOD_LSFT),  KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   S(KC_8),
+<<<<<<< Updated upstream
         KC_HYPR,        KC_RCTL, KC_LALT,KC_LGUI,TD(TD_LANG),
+=======
+        KC_HYPR,        KC_RCTL, KC_LALT,KC_LGUI,KC_LANG1,
+>>>>>>> Stashed changes
                                             TG(MEDIA),    KC_PGUP,
                                                           KC_PGDN,
                                     KC_SPC, OSM(MOD_LSFT),LALT(KC_LSFT),
@@ -87,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_FN2,     KC_Y,     KC_U  ,  KC_I,     KC_O,    KC_P,   KC_FN2,
                          KC_H,     KC_J,    KC_K,     KC_L,    KC_FN2, KC_FN2,
              S(KC_9),    KC_N,     KC_M,    KC_FN2,   KC_FN2,  KC_SLSH,KC_FN2,
-                                   KC_HENK, KC_LEFT,  KC_DOWN, KC_UP,  KC_RGHT,
+                                   KC_LANG2, KC_LEFT,  KC_DOWN, KC_UP,  KC_RGHT,
              KC_DEL,        TG(SYMBOL),
              KC_BSPC,
              KC_RALT,KC_LCTL, KC_ENT
