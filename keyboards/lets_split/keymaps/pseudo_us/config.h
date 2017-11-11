873750285e4a71d663133193b867413d87575730
Toshiki Yagi <mrabbit@usagigoya.jp>
2017-11-11 12:55:24 +0900

    enable rgb underglow

--- keyboards/lets_split/keymaps/pseudo_us/config.h
+++ keyboards/lets_split/keymaps/pseudo_us/config.h
@@ -28,6 +28,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL
 // #define USE_I2C
 
+#undef RGBLED_NUM
+
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 10
+
+
 /* Select hand configuration */
 
 #define MASTER_LEFT
--- keyboards/lets_split/keymaps/pseudo_us/rules.mk
+++ keyboards/lets_split/keymaps/pseudo_us/rules.mk
@@ -3,5 +3,7 @@ ifndef QUANTUM_DIR
 endif
 COMMAND_ENABLE          = yes # Commands for debug and configuration
 TAP_DANCE_ENABLE		= yes # Tap Dance feature
+MOUSEKEY_ENABLE			= no  # This keymap dont use mousekey.
+RGBLIGHT_ENABLE			= yes # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
 
 SRC += action_pseudo_lut.c
