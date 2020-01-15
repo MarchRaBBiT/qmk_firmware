9ac5051be576277d4e33307f72d7c722284df144
Toshiki Yagi <mrabbit@usagigoya.jp>
2020-01-15 13:12:49 +0900

    add rev2

---
+++ keyboards/rna/boards/RNA_STM32_F072Cx/board.mk
@@ -0,0 +1,5 @@
+# List of all the board related files.
+BOARDSRC = $(BOARD_PATH)/boards/RNA_STM32_F072Cx/board.c
+
+# Required include directories
+BOARDINC = $(BOARD_PATH)/boards/RNA_STM32_F072Cx
---
+++ keyboards/rna/rna.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef KEYBOARD_rna_rev1
+#include "rev1.h"
+#endif
+
+#ifdef KEYBOARD_rna_rev2
+#include "rev2.h"
+#endif
+
+#include "quantum.h"
